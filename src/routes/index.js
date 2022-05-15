7a92c2983006485f36d8b5b54a573a3fbc83d5d2
Arnold Esquivel <arnold_526@outlook.com>
2022-05-15 10:26:40 -0500

    Se agrega Event-Router al index principal de ruteo

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const groupRouter = require('./groups');
 const genderRouter = require('./genders');
 const authRouter = require('./auth');
 const reviewRouter = require('./review');
+const eventRouter = require('./event');
 
 const apiRouter = (app) => {
     app.use('/groups', groupRouter);
     app.use('/genders', genderRouter);
     app.use('/auth', authRouter);
     app.use('/reviews', reviewRouter);
+    app.use('/events', eventRouter);
 }
 
 module.exports = apiRouter;
